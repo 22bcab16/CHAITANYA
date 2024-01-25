91bc2f60a3836cd28fffe812befadfb9e8e1e699
22bcab16 <155622694+22bcab16@users.noreply.github.com>
2024-01-25 14:22:24 +0530

    Update add.c

--- add.c
+++ add.c
@@ -11,7 +11,7 @@ int main() {
     
     sum = num1 + num2+num3+num4;
     
-    printf("The sum of %d and %d is %d.", num1, num2, sum);
+    printf("The sum of %d and %d is %d.", num1, num2,num3,num4, sum);
     
     return 0;
 }
